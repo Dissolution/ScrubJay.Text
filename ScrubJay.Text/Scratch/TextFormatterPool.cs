39c824b3215f500c4e86bf2213244f9c694f6232
Timothy Jay <TimothyRJay@gmail.com>
2024-02-26 14:51:41 -0800

    Tabula Rasa

--- ScrubJay.Text/Scratch/TextFormatterPool.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ScrubJay.Text.Scratch;
-
-public sealed class TextFormatterPool
-{
-    public TextFormatterOptions DefaultOptions { get; set; } = new();
-
-    public ValueFormatters DefaultFormatters { get; set; } = new();
-}
